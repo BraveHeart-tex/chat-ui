1dc6b8e731f12f83263cec1a62af793fb1a34f6e
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2025-11-16 14:23:57 +0300

    feat: add PrimeVue with Aura theme for styling

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from 'primevue/button'
+</script>
 
-<template></template>
+<template>
+  <Button label="Button" class="" />
+</template>
